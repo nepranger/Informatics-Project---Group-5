e7ad7187c04cdf3dd4c1305a585c58f4ce9a1d96
nepranger <38054755+nepranger@users.noreply.github.com>
2018-04-24 20:08:42 -0500

    Update tables.sql
    Added more accounts/course data

--- tables.sql
+++ tables.sql
@@ -17,10 +17,27 @@ CREATE TABLE Account(
 );
 
 --Insert some data into the Account Table
-INSERT INTO Account (hawk_ID, hashedpass, name, administrator) VALUES ("nepranger", "$2a$12$HId5u4IAGIpk9BgcxuGhz.EPkIc6jk.wqc/TeYWjf0NLU6W8byZwm", "Nathan Pranger", 0);
-INSERT INTO Account (hawk_ID, hashedpass, name, administrator) VALUES ("csadmin", "$2a$12$HId5u4IAGIpk9BgcxuGhz.EPkIc6jk.wqc/TeYWjf0NLU6W8byZwm", "Juan Pablo Hourcade", 1);
-INSERT INTO Account (hawk_ID, hashedpass, name, administrator) VALUES ("segre", "$2a$12$HId5u4IAGIpk9BgcxuGhz.EPkIc6jk.wqc/TeYWjf0NLU6W8byZwm", "Alberto Segre", 0);
-INSERT INTO Account (hawk_ID, hashedpass, name, administrator) VALUES ("bwright", "$2a$12$HId5u4IAGIpk9BgcxuGhz.EPkIc6jk.wqc/TeYWjf0NLU6W8byZwm", "Bob Wright", 0);
+INSERT INTO Account (hawk_ID, hashedpass, name, administrator) VALUES ("nepranger", "$2a$12$HId5u4IAGIpk9BgcxuGhz.EPkIc6jk.wqc/TeYWjf0NLU6W8byZwm", "Nathan Pranger", 0); --Student
+INSERT INTO Account (hawk_ID, hashedpass, name, administrator) VALUES ("csadmin", "$2a$12$HId5u4IAGIpk9BgcxuGhz.EPkIc6jk.wqc/TeYWjf0NLU6W8byZwm", "Juan Pablo Hourcade", 1); --Admin
+INSERT INTO Account (hawk_ID, hashedpass, name, administrator) VALUES ("segre", "$2a$12$HId5u4IAGIpk9BgcxuGhz.EPkIc6jk.wqc/TeYWjf0NLU6W8byZwm", "Alberto Segre", 0); --Professor/Faculty
+INSERT INTO Account (hawk_ID, hashedpass, name, administrator) VALUES ("bwright", "$2a$12$HId5u4IAGIpk9BgcxuGhz.EPkIc6jk.wqc/TeYWjf0NLU6W8byZwm", "Bob Wright", 0); --Tutor
+INSERT INTO Account (hawk_ID, hashedpass, name, administrator) VALUES ("dsmith", "$2a$12$HId5u4IAGIpk9BgcxuGhz.EPkIc6jk.wqc/TeYWjf0NLU6W8byZwm", "Dan Smith", 0); --Student
+INSERT INTO Account (hawk_ID, hashedpass, name, administrator) VALUES ("krenshaw", "$2a$12$HId5u4IAGIpk9BgcxuGhz.EPkIc6jk.wqc/TeYWjf0NLU6W8byZwm", "Kallie Renshaw", 0); --Student
+INSERT INTO Account (hawk_ID, hashedpass, name, administrator) VALUES ("ihoggatt", "$2a$12$HId5u4IAGIpk9BgcxuGhz.EPkIc6jk.wqc/TeYWjf0NLU6W8byZwm", "Izola Hoggatt", 0); --Student
+INSERT INTO Account (hawk_ID, hashedpass, name, administrator) VALUES ("jsanger", "$2a$12$HId5u4IAGIpk9BgcxuGhz.EPkIc6jk.wqc/TeYWjf0NLU6W8byZwm", "Jerome Sanger", 0); --Student
+INSERT INTO Account (hawk_ID, hashedpass, name, administrator) VALUES ("bmcclintock", "$2a$12$HId5u4IAGIpk9BgcxuGhz.EPkIc6jk.wqc/TeYWjf0NLU6W8byZwm", "Bethany Mcclintock", 0); --Student
+INSERT INTO Account (hawk_ID, hashedpass, name, administrator) VALUES ("fbarone", "$2a$12$HId5u4IAGIpk9BgcxuGhz.EPkIc6jk.wqc/TeYWjf0NLU6W8byZwm", "Felice Barone", 0); --Student
+INSERT INTO Account (hawk_ID, hashedpass, name, administrator) VALUES ("nbueno", "$2a$12$HId5u4IAGIpk9BgcxuGhz.EPkIc6jk.wqc/TeYWjf0NLU6W8byZwm", "Nicolette Bueno", 0); --Student
+INSERT INTO Account (hawk_ID, hashedpass, name, administrator) VALUES ("alingle", "$2a$12$HId5u4IAGIpk9BgcxuGhz.EPkIc6jk.wqc/TeYWjf0NLU6W8byZwm", "Alina Lingle", 0); --Student
+INSERT INTO Account (hawk_ID, hashedpass, name, administrator) VALUES ("nreddell", "$2a$12$HId5u4IAGIpk9BgcxuGhz.EPkIc6jk.wqc/TeYWjf0NLU6W8byZwm", "Nedra Reddell", 0); --Tutor
+INSERT INTO Account (hawk_ID, hashedpass, name, administrator) VALUES ("fkeas", "$2a$12$HId5u4IAGIpk9BgcxuGhz.EPkIc6jk.wqc/TeYWjf0NLU6W8byZwm", "Felipe Keas", 0); --Tutor
+INSERT INTO Account (hawk_ID, hashedpass, name, administrator) VALUES ("lmendelson", "$2a$12$HId5u4IAGIpk9BgcxuGhz.EPkIc6jk.wqc/TeYWjf0NLU6W8byZwm", "Lashell Mendelson", 0); --Tutor
+INSERT INTO Account (hawk_ID, hashedpass, name, administrator) VALUES ("tbaril", "$2a$12$HId5u4IAGIpk9BgcxuGhz.EPkIc6jk.wqc/TeYWjf0NLU6W8byZwm", "Tim Baril", 0); --Tutor
+INSERT INTO Account (hawk_ID, hashedpass, name, administrator) VALUES ("emcafee", "$2a$12$HId5u4IAGIpk9BgcxuGhz.EPkIc6jk.wqc/TeYWjf0NLU6W8byZwm", "Edmond Mcafee", 0); --Faculty
+INSERT INTO Account (hawk_ID, hashedpass, name, administrator) VALUES ("rholzman", "$2a$12$HId5u4IAGIpk9BgcxuGhz.EPkIc6jk.wqc/TeYWjf0NLU6W8byZwm", "Richard Holzman", 0); --Faculty
+INSERT INTO Account (hawk_ID, hashedpass, name, administrator) VALUES ("wfleischmann", "$2a$12$HId5u4IAGIpk9BgcxuGhz.EPkIc6jk.wqc/TeYWjf0NLU6W8byZwm", "William Fleischmann", 0); --Faculty
+INSERT INTO Account (hawk_ID, hashedpass, name, administrator) VALUES ("fbirch", "$2a$12$HId5u4IAGIpk9BgcxuGhz.EPkIc6jk.wqc/TeYWjf0NLU6W8byZwm", "Francie Birch", 0); --Faculty
+
 
 CREATE TABLE Course(
     course_ID VARCHAR(120) NOT NULL,
@@ -31,7 +48,17 @@ CREATE TABLE Course(
 );
 
 --Insert some data into the Course Table
-INSERT INTO Course (course_ID, course_name, course_number, course_section) VALUES ("CS:2200", "Operating Systems", "2200", "001" );
+INSERT INTO Course (course_ID, course_name, course_number, course_section) VALUES ("CS:1020", "Principles of Computing", "1020", "001" );
+INSERT INTO Course (course_ID, course_name, course_number, course_section) VALUES ("CS:1110", "Introduction to Computer Science", "1110", "001" );
+INSERT INTO Course (course_ID, course_name, course_number, course_section) VALUES ("CS:1210", "Computer Science 1: Fundamentals", "1210", "001" );
+INSERT INTO Course (course_ID, course_name, course_number, course_section) VALUES ("CS:2100", "Programming for Informatics", "2100", "001" );
+INSERT INTO Course (course_ID, course_name, course_number, course_section) VALUES ("CS:2210", "Discrete Structures", "2210", "001" );
+INSERT INTO Course (course_ID, course_name, course_number, course_section) VALUES ("CS:2520", "Human Computer Interaction", "2520", "001" );
+INSERT INTO Course (course_ID, course_name, course_number, course_section) VALUES ("CS:2620", "Networking and Security for Informatics", "2620", "001" );
+INSERT INTO Course (course_ID, course_name, course_number, course_section) VALUES ("CS:3330", "Algorithms", "3330", "001" );
+INSERT INTO Course (course_ID, course_name, course_number, course_section) VALUES ("CS:3620", "Operating Systems", "3620", "001" );
+INSERT INTO Course (course_ID, course_name, course_number, course_section) VALUES ("CS:3980", "Topics in Computer Science", "3980", "001" );
+
 
 
 --Create the Student Table 
@@ -45,7 +72,7 @@ CREATE TABLE Student(
 );
 
 --Insert some data into the Student Table
-INSERT INTO Student (hawk_ID, course_ID, budget) VALUES ("nepranger", "CS:2200", "4");
+INSERT INTO Student (hawk_ID, course_ID, budget) VALUES ("nepranger", "CS:2620", "4");
 
 --Create the Tutor Table
 
@@ -58,7 +85,7 @@ CREATE TABLE Tutor(
 );
 
 --Insert some data into the Tutor Table
-INSERT INTO Tutor (hawk_ID, course_ID, hours_per_week) VALUES ("bwright", "CS:2200", "10");
+INSERT INTO Tutor (hawk_ID, course_ID, hours_per_week) VALUES ("bwright", "CS:2620", "10");
 
 --Create the Faculty Table
 
@@ -71,12 +98,7 @@ CREATE TABLE Faculty(
 );
 
 --Insert some data into the Faculty Table
-INSERT INTO Faculty (hawk_ID, course_ID) VALUES ("segre", "CS:2200");
-
---Create the Course Table
-
-
-
+INSERT INTO Faculty (hawk_ID, course_ID) VALUES ("segre", "CS:1110");
 
 
 --Create the Problem Set Table
