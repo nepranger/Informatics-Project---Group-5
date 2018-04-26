19a6b64028b23e0ed9f2349dd3a0af42841a1ae6
nepranger <38054755+nepranger@users.noreply.github.com>
2018-04-26 16:23:17 -0500

    Update isloggedin.php

--- isloggedin.php
+++ isloggedin.php
@@ -1,41 +1,26 @@
 <?php
 
-//we need to include these two files in order to work with the datbase
-include_once('config.php');
-include_once('dbutils.php');
+//log in user by checking whether the session variable username is set
 
-//Get a connection to the database
-$db = connectDB($DBHost, $DBUser, $DBPassword, $DBName);
+$isloggedin = false;
+$hawk_ID = "not logged in";
 
 session_start();
-$hawk_ID = $_SESSION['username'];
-
-//Set up a query to get information on movies
-$query = "SELECT Course.course_ID, Course.course_name, Course.course_number, Course.course_section FROM Course, Student WHERE Student.hawk_ID = '$hawk_ID' AND Student.course_ID = Course.course_ID";
-
-    //course_ID VARCHAR(120) NOT NULL,
-    //course_name VARCHAR(255) NOT NULL,
-    //course_number VARCHAR(120) NOT NULL,
-    //course_section VARCHAR(255) NOT NULL,
-
-//run the query to get info on players
-$result = queryDB($query, $db);
-
-//assign results to an array we can then send back
-$courses = array();
-$i = 0;
-
-// go through the results one by one
-while ($currCourse = nextTuple($result)) {
-    $courses[$i] = $currCourse;
-    $i++;
+if (isset($_SESSION['username'])) {
+    //if the session variable username is set, then we are logged in
+    $isloggedin = true;
+    $hawk_ID = $_SESSION['username'];  
 }
 
-//put together a JSON object to send back the data on the students
+//send response back
 $response = array();
 $response['status'] = 'success';
-$response['value']['courses'] = $courses;
+$response['loggedin'] = $isloggedin;
+$response['username'] = $hawk_ID;
 header('Content-Type: application/json');
 echo(json_encode($response));
+
+
+
 
 ?>
