3c86f4d26cf2043310d9f0ddee0aa096b7adb169
nepranger <38054755+nepranger@users.noreply.github.com>
2018-04-26 16:24:25 -0500

    Create getStudent.php

---
+++ getStudent.php
@@ -0,0 +1,37 @@
+<?php
+
+
+
+//we need to include these two files in order to work with the datbase
+include_once('config.php');
+include_once('dbutils.php');
+
+//Get a connection to the database
+$db = connectDB($DBHost, $DBUser, $DBPassword, $DBName);
+
+//Set up a query to get information on movies
+$query = "SELECT * FROM Student";
+
+//run the query to get info on players
+$result = queryDB($query, $db);
+
+//assign results to an array we can then send back
+$students = array();
+$i = 0;
+
+// go through the results one by one
+while ($currStudent = nextTuple($result)) {
+    $students[$i] = $currStudent;
+    $i++;
+}
+
+//put together a JSON object to send back the data on the students
+$response = array();
+$response['status'] = 'success';
+$response['value']['students'] = $students;
+header('Content-Type: application/json');
+echo(json_encode($response));
+
+
+
+?>
