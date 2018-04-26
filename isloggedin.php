a2f5a969551465819cdf880cd3e67f4ab8690d4a
nepranger <38054755+nepranger@users.noreply.github.com>
2018-04-26 16:22:51 -0500

    Update isloggedin.php

--- isloggedin.php
+++ isloggedin.php
@@ -1,54 +1,41 @@
-<<<<<<< HEAD
 <?php
 
-//log in user by checking whether the session variable username is set
+//we need to include these two files in order to work with the datbase
+include_once('config.php');
+include_once('dbutils.php');
 
-$isloggedin = false;
-$username = "not logged in";
+//Get a connection to the database
+$db = connectDB($DBHost, $DBUser, $DBPassword, $DBName);
 
 session_start();
-if (isset($_SESSION['username'])) {
-    //if the session variable username is set, then we are logged in
-    $isloggedin = true;
-    $username = $_SESSION['username'];  
+$hawk_ID = $_SESSION['username'];
+
+//Set up a query to get information on movies
+$query = "SELECT Course.course_ID, Course.course_name, Course.course_number, Course.course_section FROM Course, Student WHERE Student.hawk_ID = '$hawk_ID' AND Student.course_ID = Course.course_ID";
+
+    //course_ID VARCHAR(120) NOT NULL,
+    //course_name VARCHAR(255) NOT NULL,
+    //course_number VARCHAR(120) NOT NULL,
+    //course_section VARCHAR(255) NOT NULL,
+
+//run the query to get info on players
+$result = queryDB($query, $db);
+
+//assign results to an array we can then send back
+$courses = array();
+$i = 0;
+
+// go through the results one by one
+while ($currCourse = nextTuple($result)) {
+    $courses[$i] = $currCourse;
+    $i++;
 }
 
-//send response back
+//put together a JSON object to send back the data on the students
 $response = array();
 $response['status'] = 'success';
-$response['loggedin'] = $isloggedin;
-$response['username'] = $username;
+$response['value']['courses'] = $courses;
 header('Content-Type: application/json');
 echo(json_encode($response));
 
-
-
-
-=======
-<?php
-
-//log in user by checking whether the session variable username is set
-
-$isloggedin = false;
-$username = "not logged in";
-
-session_start();
-if (isset($_SESSION['username'])) {
-    //if the session variable username is set, then we are logged in
-    $isloggedin = true;
-    $username = $_SESSION['username'];  
-}
-
-//send response back
-$response = array();
-$response['status'] = 'success';
-$response['loggedin'] = $isloggedin;
-$response['username'] = $username;
-header('Content-Type: application/json');
-echo(json_encode($response));
-
-
-
-
->>>>>>> d05e94baa4d18daaa065144df5d1fe02991494d1
 ?>
